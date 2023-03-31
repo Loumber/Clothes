d04b290eefd8403fc793902476eafbd288606f79
Loumber <92634780+Loumber@users.noreply.github.com>
2023-03-31 18:28:53 +0300

    !

--- packages/open_meteo_api/lib/open_meteo_api.dart
+++ packages/open_meteo_api/lib/open_meteo_api.dart
@@ -1,7 +1,7 @@
 library open_meteo_api;
 
-export '../weather_repository/lib/src/models/models.dart';
-
+export '../../weather_repository/lib/src/models/models.dart';
+export '../../weather_repository/lib/src/models/weather.dart';
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
