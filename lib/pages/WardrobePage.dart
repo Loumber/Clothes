baa639a484a61d605e4ff17010bf490b4c367b4d
PiggyJesus <Kulakov.Stanislav82@gmail.com>
2023-05-06 23:07:44 +0300

    Очень важное изменение

--- lib/pages/WardrobePage.dart
+++ lib/pages/WardrobePage.dart
@@ -37,7 +37,7 @@ class WardrobePage extends StatelessWidget {
               TableRow(
                   children: [
                     Tile(RouteArguments('Верхняя одежда'), Clothes.jacket),
-                    Tile(RouteArguments('Все'), Clothes.jacket),
+                    Tile(RouteArguments('Всё'), Clothes.jacket),
                   ]
               )
             ],
